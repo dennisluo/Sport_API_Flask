a6843407d1a60dcb240299cba00f576d332b1b83
dennisluo <dennisluo95@gmail.com>
2021-01-17 20:54:31 +0000

    Return All Results Endpoint
    Return all match results for nba & bpl if accessing main endpoint (/sport_api/v1)

--- app/main/sport.py
+++ app/main/sport.py
@@ -1,8 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from datetime import datetime
+from app.api_call import api_call
+import json
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/', methods=['GET'])
+@main.route('/sport_api/v1', methods=['GET'])
 def sport():
-    return 'Hello Flask'
+    # Call api and retrieve all match results
+    games = api_call()
+    games = json.dumps(games)
+
+    return games
+
+
+
+
+if __name__ == '__main__':
+    main.run(debug=True)
